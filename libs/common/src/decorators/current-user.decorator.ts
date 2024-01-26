7494a42a2ece7b7ffd26c375a111c8015cc27cb2
Anna Krasilnikova <akr6363@mail.ru>
2024-01-26 19:38:20 +0300

    get current user, update schema

---
+++ libs/common/src/decorators/current-user.decorator.ts
@@ -0,0 +1,12 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { JwtPayload } from '@auth/interfaces';
+
+export const CurrentUser = createParamDecorator(
+  (
+    key: keyof JwtPayload,
+    ctx: ExecutionContext,
+  ): JwtPayload | Partial<JwtPayload> => {
+    const req = ctx.switchToHttp().getRequest();
+    return key ? req.user[key] : req.user;
+  },
+);
