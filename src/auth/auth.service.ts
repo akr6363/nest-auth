ec30fa637db684ac4864083a4e071a2581aa276e
Anna Krasilnikova <akr6363@mail.ru>
2024-02-14 12:11:50 +0300

    fix refreshTokens()

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -26,22 +26,17 @@ export class AuthService {
   ) {}
 
   async refreshTokens(refreshToken: string, agent: string): Promise<Tokens> {
-    const token = await this.prismaService.token.findUnique({
+    const token = await this.prismaService.token.delete({
       where: { token: refreshToken },
     });
-    if (!token) {
-      throw new UnauthorizedException();
-    }
-    await this.prismaService.token.delete({
-      where: { token: refreshToken },
-    });
-    if (new Date(token.exp) < new Date()) {
+    if (!token || new Date(token.exp) < new Date()) {
       throw new UnauthorizedException();
     }
     const user = await this.userService.findOne(token.userId);
 
     return this.generateTokens(user, agent);
   }
+
   async register(dto: RegisterDto) {
     const user: User = await this.userService
       .findOne(dto.email)
